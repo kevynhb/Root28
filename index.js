973914d43b63f5dd0c4e8806bb4ce3e41d678f64
Kevyn Hotts <kevynh36@gmail.com>
2022-01-09 16:25:20 -0400

    inicio projeto, treinando lógica

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+  Escreva um programa que leia as notas das duas avaliações normais e a nota da avaliação optativa.
+  Caso o aluno não tenha feito a optativa deve ser fornecido o valor -1.
+  Calcular a média do semestre considerando que a prova optativa substitui a nota mais baixa entre as duas primeiras avaliações.
+  Escrever a média e mensagens que indiquem se o aluno foi aprovado, reprovado ou está em exame, de acordo com as informações abaixo:
+
+  Se a média for maior igual a 6
+    Aprovado
+  Se a média for menor que 3
+    Reprovado 
+  Se média for maior igual a 3 e menor que 6
+    Exame
+*/
+
+const avaliacao1 = 4
+const avaliacao2 = 8
+const avaliacaoOptativa = 2
