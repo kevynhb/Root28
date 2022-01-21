d4b3836c6466cbe9a741827973f09741b3bb1d07
Kevyn Hotts <kevynh36@gmail.com>
2022-01-21 16:26:30 -0400

    error

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 /*
   Escreva um programa que leia as notas das duas avaliações normais e a nota da avaliação optativa.
+
   Caso o aluno não tenha feito a optativa deve ser fornecido o valor -1.
+
   Calcular a média do semestre considerando que a prova optativa substitui a nota mais baixa entre as duas primeiras avaliações.
+
   Escrever a média e mensagens que indiquem se o aluno foi aprovado, reprovado ou está em exame, de acordo com as informações abaixo:
 
   Se a média for maior igual a 6
@@ -12,6 +15,14 @@
     Exame
 */
 
+console.log('Inicio Aplicação')
+
 const avaliacao1 = 4
 const avaliacao2 = 8
-const avaliacaoOptativa = 2
+let avaliacaoOptativa = 0
+
+if (avaliacaoOptativa == undefined) {
+  avaliacaoOptativa = -1
+
+  console.log(avaliacaoOptativa)
+}
